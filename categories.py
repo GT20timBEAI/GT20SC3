141f2e3e50256cd672e1c238d070a8c2491f0d68
Suryana <nanasuryana335@gmail.com>
2022-11-02 21:04:44 +0700

    complete getCategories endpoint

--- categories.py
+++ categories.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request
 from utils import run_query, validUser
+import uuid
 
 
 categories_bp = Blueprint("categories", __name__, url_prefix="/categories")
@@ -7,27 +8,35 @@ categories_bp = Blueprint("categories", __name__, url_prefix="/categories")
 
 @categories_bp.route("", methods=["GET"])
 def getCategories():
-    pass
+    try:
+        get_category = run_query("select * from Category")
+        category_data = []
+        for i in get_category:
+            category_data.append(i)
+        return {"data": category_data}, 200
+    except KeyError:
+        return {"message": "error, user already exist"}
 
 
 @categories_bp.route("", methods=["POST"])
 def createCategories():
     try:
-        jwt_token = request.headers.get('token')
-        category_name = request.json.get('category_name')
+        jwt_token = request.headers.get('Authentication')
+        body = request.json
+        category = body['category_name']
 
         if not validUser(jwt_token, True):
             return {"error": "user not valid"}, 400
 
         id = uuid.uuid4()
         run_query("insert into Category(category_id, category_name) \
-            values(\"{id}\", \"{category_name}\")")
+            values(\"{id}\", \"{category_name}\"), True")
         return {"message": "Category added"}
     except KeyError:
         return {"message": "error, user already exist"}
 
 
-@categories_bp.route("/{category_id}", methods=["GET"])
+@categories_bp.route("/{category_id}", methods=["PUT"])
 def updateCategories():
     pass
 
