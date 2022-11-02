34e2c86499fec052eb2e6dadd12ba08d613574d4
Suryana <nanasuryana335@gmail.com>
2022-11-02 22:02:52 +0700

    complete getCategories with jwt token

--- categories.py
+++ categories.py
@@ -9,6 +9,11 @@ categories_bp = Blueprint("categories", __name__, url_prefix="/categories")
 @categories_bp.route("", methods=["GET"])
 def getCategories():
     try:
+        jwt_token = request.headers.get('Authentication')
+
+        if not validUser(jwt_token, True):
+            return {"error": "user not valid"}, 400
+
         get_category = run_query("select * from Category")
         category_data = []
         for i in get_category:
@@ -31,7 +36,7 @@ def createCategories():
         id = uuid.uuid4()
         run_query("insert into Category(category_id, category_name) \
             values(\"{id}\", \"{category_name}\"), True")
-        return {"message": "Category added"}
+        return {"message": "Category added"}, 200
     except KeyError:
         return {"message": "error, user already exist"}
 
