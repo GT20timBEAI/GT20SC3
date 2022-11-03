18fba85e289ef8462eb8da2422512c284e9cf97a
Suryana <nanasuryana335@gmail.com>
2022-11-03 10:18:18 +0700

    add get User Shipping Address

--- user.py
+++ user.py
@@ -1,4 +1,5 @@
-from flask import Blueprint
+from flask import Blueprint, request
+from utils import run_query
 
 
 user_bp = Blueprint("user", __name__, url_prefix="/user")
@@ -6,16 +7,30 @@ user_bp = Blueprint("user", __name__, url_prefix="/user")
 
 @user_bp.route("/shipping_address", methods=["GET"])
 def getUserShippingAddress():
-    pass
+    try:
+        user_sp = run_query("select id, name, phone_number, address, city from From Users \
+            inner join Buyer_Shipping using(user_id)")
+        data = {
+            "id": user_sp['id'],
+            "name": user_sp['name'],
+            "phone_number": user_sp['phone_number'],
+            "address": user_sp['address'],
+            "city": user_sp['city']
+        }, 200
+
+    except KeyError:
+        return {"message": "error, user already exist"}
 
 
 @user_bp.route("", methods=["GET"])
 def userDetail():
     pass
 
+
 @user_bp.route("/shipping_address", methods=["GET"])
 def changeShippingAddress():
     pass
+
 
 @user_bp.route("/balance", methods=["POST"])
 def topupBalance():
@@ -25,5 +40,3 @@ def topupBalance():
 @user_bp.route("/balance", methods=["GET"])
 def getBalance():
     pass
-
-
