7d77748046829f58b57dfe96dfccecda3f1448c0
Suryana <nanasuryana335@gmail.com>
2022-11-18 17:05:32 +0700

    repair get user and complete add user endpoint <interim>

--- cart.py
+++ cart.py
@@ -7,7 +7,16 @@ cart_bp = Blueprint("cart", __name__, url_prefix="/cart")
 @cart_bp.route("", methods=["POST"])
 def addtoCart():
     try:
-        pass
+        jwt_token = request.headers.get('Authentication')
+        body = request.json
+        id, quantity, size = body['item_id'], body['quantity'], body['size']
+
+        if not validUser(jwt_token, True):
+            return {"error": "user not valid"}, 400
+
+        run_query("INSERT INTO Cart(item_id, quantity, size)\
+            values(\"{id}\", \"{quantity}\", \"{size}\"), True")
+        return {"message": "Item added to cart"}, 200
 
     except SQLAlchemyError:
         return {"error": "user already exist"}
@@ -17,6 +26,10 @@ def addtoCart():
 def getUserCart():
     try:
         jwt_token = request.headers.get('Authentication')
+
+        if not validUser(jwt_token, True):
+            return {"error": "user not valid"}, 400
+
         cart_data = run_query("SELECT c.Cart_id, c.quantity, c.size, c.item_id p.price, p.image_url, p.product_name\
                               FROM Cart c\
                               INNER JOIN Product_List p\
