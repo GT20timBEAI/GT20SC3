3c161f441331e407bbbd2caa4c6617d3fde11626
suryana <nanasuryana335@gmail.com>
2022-11-22 14:22:58 +0000

    complete change shipping address and add jwt token to get user shipping address

--- user.py
+++ user.py
@@ -8,6 +8,12 @@ user_bp = Blueprint("user", __name__, url_prefix="/user")
 @user_bp.route("/shipping_address", methods=["GET"])
 def getUserShippingAddress():
     try:
+
+        jwt_token = request.headers.get('Authentication')
+
+        if not validUser(jwt_token, True):
+            return {"error": "user not valid"}, 400
+            
         user_sp = run_query("select id, name, phone_number, address, city from From Users \
             inner join Buyer_Shipping using(user_id)")
         data = {
@@ -16,7 +22,8 @@ def getUserShippingAddress():
             "phone_number": user_sp['phone_number'],
             "address": user_sp['address'],
             "city": user_sp['city']
-        }, 200
+        }
+        return data, 200
 
     except KeyError:
         return {"message": "error, user already exist"}
@@ -44,7 +51,32 @@ def userDetail():
 
 @user_bp.route("/shipping_address", methods=["POST"])
 def changeShippingAddress():
-    pass
+    try:
+        jwt_token = request.headers.get('Authentication')
+        body = request.json
+        name, phone_number, address, city = body['name'], body['phone_number'], body['address'], body['city']
+
+        if not validUser(jwt_token, True):
+            return {"error": "user not valid"}, 400
+        
+        run_query(f'UPDATE Users set name=\'{name}\', phone_number=\'{phone_number}\'', True)
+        run_query(f'UPDATE Buyer_Shipping set address=\'{address}\', city=\'{city}\'', True)
+
+        shipping_addresss = run_query('SELECT u.name, u.phone number, b.address, b.city \
+        FROM Users u\
+        INNER JOIN Buyer_Shipping b\
+        ON b.user_id = u.\'"id"\';')
+
+        data = {
+            "name": shipping_addresss['name'],
+            "phone_number": shipping_addresss['phone_number'],
+            "address": shipping_addresss['addess'],
+            "city": shipping_addresss['city']
+        }
+        return data, 200
+
+    except KeyError:
+        return {"message": "User already exist"}
 
 
 @user_bp.route("/balance", methods=["POST"])
