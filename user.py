528a3381c47bb632c5386cfe26a7b70750157fb0
Suryana <nanasuryana335@gmail.com>
2022-11-29 09:14:52 +0700

    fixing change user shipping address

--- user.py
+++ user.py
@@ -13,7 +13,7 @@ def getUserShippingAddress():
 
         if not validUser(jwt_token, True):
             return {"error": "user not valid"}, 400
-            
+
         user_shipping = run_query(f"""
         SELECT u.id, u.name, u.phone_number u.token, by.address, by.city
         FROM Users u
@@ -49,7 +49,7 @@ def userDetail():
         FROM Users
         WHERE token = \'{jwt_token}\'
         """)[0]
-        
+
         user_Data = {
             "name": userDetail[0]['name'],
             "email": userDetail[0]['email'],
@@ -71,41 +71,32 @@ def changeShippingAddress():
 
         if not validUser(jwt_token, True):
             return {"error": "user not valid"}, 400
-        
-        run_query(f'UPDATE Users set name=\'{name}\', phone_number=\'{phone_number}\' WHERE token = \'{jwt_token}\' ', True)
 
-        userID = run_query(f"""
-        SELECT us.user_id, u.id, u.token FROM Buyer_Shipping
-        INNER JOIN Users u
-        ON u.id = us.user_id
-        WHERE u.token = \'{jwt_token}\'
-        """)[0]
+        id = run_query(f"""
+        SELECT id FROM "Users"
+        where token = '{jwt_token}'
+        """)[0]['id']
 
-        for i in userID:
-            run_query(f'UPDATE Buyer_Shipping set address=\'{address}\', city=\'{city}\' WHERE user_id = \'{i['us.user_id']}\'', True)
+        body = request.json
+        address = body['address']
+        city = body['city']
+        name = body['name']
+        phone = body['phone_number']
 
-        ##################################################
-        # # userId = run_query(f"""
-        # # SELECT id FROM Users
-        # # WHERE token = \'{jwt_token}\''
-        # # """)[0]
+        # ceck phone number
+        if symbol(phone):
+            return {"message": "phone just containt number"}, 400
 
-        # run_query(f'UPDATE Buyer_Shipping set address=\'{address}\', city=\'{city}\' WHERE user_id = \'{userId}\'', True)
-        ##################################################
-
-        shipping_addresss = run_query(f'SELECT u.name, u.phone number, u.token, b.address, b.city \
-        FROM Users u\
-        INNER JOIN Buyer_Shipping b\
-        ON b.user_id = u.id\
-        WHERE u.token = \'{jwt_token}\'')[0]
-
-        data = {
-            "name": shipping_addresss['name'],
-            "phone_number": shipping_addresss['phone_number'],
-            "address": shipping_addresss['addess'],
-            "city": shipping_addresss['city']
-        }
-        return data, 200
+        run_query(f"""
+        Insert into "Buyer_Shipping" (user_id, address, city, name, phone_number)
+        VALUES('{id}', '{address}', '{city}', '{name}' , {int(phone)})
+        """, True)
+        return {
+            "name": name,
+            "phone_number": phone,
+            "address": address,
+            "city": city
+        }, 200
 
     except KeyError:
         return {"message": "User already exist"}
